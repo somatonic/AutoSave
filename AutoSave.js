ec8b074ecdc499def3511570d9ce658d2a6a1ce5
somatonic <philipp@urlich.ch>
2014-01-01 10:00:44 +0100

    added support for CKEditor inline mode

--- AutoSave.js
+++ AutoSave.js
@@ -11,12 +11,21 @@
         $("body").append($error);
 
         window.setInterval(function(){
+
             if(window.tinyMCE !== undefined){
                 tinyMCE.triggerSave();
             }
+
             if(window.CKEDITOR !== undefined) {
                 for ( instance in CKEDITOR.instances ) CKEDITOR.instances[instance].updateElement();
             }
+
+            // for all those inline ckeditor users
+            $("form.InputfieldForm").find('.InputfieldCKEditorInline').each(function() {
+                var $input = $(this).next('input');
+                var value = $(this).html();
+                $input.attr('value', value);
+            });
 
             // serialize form and append page id
             var $data = $('form#ProcessPageEdit').serialize() + "&id=" + $('#PageIDIndicator').text();
@@ -45,28 +54,6 @@
 
         }, config.autosave_interval * 1000);
 
-        //var $link = $('<a href="#">AJAX SAVE</a>');
-        // $link.on('click',function(e){
-        //     e.preventDefault();
-        //     // trigger Wywiwyg's so we get the actual edits for posting
-        //     tinyMCE.triggerSave();
-        //     for ( instance in CKEDITOR.instances ) CKEDITOR.instances[instance].updateElement();
-
-        //     // serialize form and append page id
-        //     var $data = $('form#ProcessPageEdit').serialize() + "&id=" + $('#PageIDIndicator').text();
-
-        //     //console.log($data);
-
-        //     $.ajax({
-        //         url: config.urls.admin + 'page/autosave-page-edit/ajaxsave/',
-        //         type: 'post',
-        //         data: $data,
-        //         success: function(data){
-        //             console.log(data);
-        //         }
-        //     });
-        // });
-        // $("body").prepend($link);
     });
 
 
